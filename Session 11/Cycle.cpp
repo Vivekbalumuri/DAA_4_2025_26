7ee6b9ed60bd3db8f34219d5775bd2c602a71335
BALUMURI VIVEK GOWD <balumurivivek2678@gmail.com>
2026-01-30 11:05:41 +0530

    Update Cycle.cpp

--- Session 11/Cycle.cpp
+++ Session 11/Cycle.cpp
@@ -1,39 +1,45 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
 vector<vector<int>> adjlist;
 vector<int> path;
 vector<bool> visited;
 int v;
 
-bool hasedge(int u,int v){
-    for(int x:adjlist[u]){
-        if(x==v) return true;
+bool hasedge(int u,int v)
+{
+    for(int x:adjlist[u])
+    {
+        if(x == v) return true;
     }
     return false;
 }
 
-bool solve(int u,int cnt){
-    if(cnt==v){
+bool solve(int u,int cnt)
+{
+    if(cnt == v)
+    {
         return hasedge(u,0);
     }
-    for(int x:adjlist[u]){
-        if(!visited[x]){
-            visited[x]=true;
+    for(int x:adjlist[u])
+    {
+        if(!visited[x])
+        {
+            visited[x] = true;
             path.push_back(x);
             if(solve(x,cnt+1)) return true;
-            visited[x]=false;
+            visited[x] = false;
             path.pop_back();
         }
     }
     return false;
 }
 
-int main(){
-    v=5;
-    adjlist={
-        {1,3},
+int main()
+{
+    v = 5;
+    adjlist = {
+        {1,2},
         {0,2,3,4},
         {1,4},
         {0,1,4},
@@ -42,13 +48,15 @@ int main(){
     visited.assign(v,false);
     path.clear();
     path.push_back(0);
-    visited[0]=true;
-    if(solve(0,1)){
-        for(int i:path) cout<<i<<" ";
-        cout<<0;
+    visited[0] = true;
+    if(solve(0,1))
+    {
+        for(int i:path) cout << i << " ";
+        cout << 0;
     }
-    else{
-        cout<<"NO PATH"<<endl;
+    else
+    {
+        cout << "No Path";
     }
     return 0;
 }
